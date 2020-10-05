5869f72776de997f50015a66054640d7f0f5b8cc
irit2607 <67689773+irit2607@users.noreply.github.com>
2020-10-05 22:53:30 +0530

    Update main.py

--- Krishi Kalyan/main.py
+++ Krishi Kalyan/main.py
@@ -3,28 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from werkzeug.utils import secure_filename
 from flask_mail import Mail
+from sqlalchemy import or_
 import os
 import math
 import json
 from datetime import datetime
 
-with open('config.json', 'r') as c:
-    params = json.load(c)["params"]
-
-local_server = True
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
 
-if(local_server):
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://KKKoderz:papa2202@KKKoderz.mysql.pythonanywhere-services.com/KKKoderz$dbkrishikalyan"
 
 db = SQLAlchemy(app)
-if(local_server):
-    engine = create_engine(params['local_uri'])
-else:
-    engine = create_engine(params['prod_uri'])
+engine = create_engine("mysql+pymysql://KKKoderz:papa2202@KKKoderz.mysql.pythonanywhere-services.com/KKKoderz$dbkrishikalyan")
 connection=engine.raw_connection()
 
 class Contacts(db.Model):
@@ -52,21 +44,21 @@ class Scheme(db.Model):
 
 @app.route("/")
 def home():
-	return render_template('index.html', params=params)
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
-    return render_template('about.html', params=params)
+    return render_template('about.html')
 
 @app.route("/schemes")
 def schemes():
     schemes = Scheme.query.filter_by().all()
-    last = math.ceil(len(schemes)/int(params['no_of_schemes']))    
+    last = math.ceil(len(schemes)/5)
     page = request.args.get('page')
     if(not str(page).isnumeric()):
         page = 1
     page= int(page)
-    schemes = schemes[(page-1)*int(params['no_of_schemes']): (page-1)*int(params['no_of_schemes'])+ int(params['no_of_schemes'])]
+    schemes = schemes[(page-1)*5: (page-1)*5+ 5]
     if (page==1):
         prev = "#"
         next = "?page="+ str(page+1)
@@ -77,92 +69,77 @@ def schemes():
         prev = "?page=" + str(page - 1)
         next = "?page=" + str(page + 1)
 
-    return render_template('schemes.html', params=params, schemes=schemes, prev=prev, next=next)
+    return render_template('schemes.html', schemes=schemes, prev=prev, next=next)
 
 @app.route("/scheme/<string:scheme_slug>", methods=['GET'])
 def scheme_route(scheme_slug):
     scheme = Scheme.query.filter_by(slug=scheme_slug).first()
-    return render_template('scheme.html', params=params, scheme=scheme)
+    return render_template('scheme.html', scheme=scheme)
 
 @app.route("/find")
 def find():
-    return render_template('find.html', params=params)
+    return render_template('find.html')
 
 @app.route("/result", methods = ['GET', 'POST'])
-def result():    
-    mycursor=connection.cursor()
+def result():
+    # mycursor=connection.cursor()
     if request.method=='POST':
         result = request.form
         a = result['age']
         if 18<=int(a)<=40:
             a="18"
         b = result['landsize']
-        if b == "Less than 2 acres":
-            b = "2"
-        elif b == "Less than 5 acres":
-            b = "5"
-        else: 
-            b = "1"        
-        mycursor.execute("select name,des,slug from scheme where age='"+a+"' or landsize='"+b+"'")
-        a = mycursor.fetchall()
-        b = one()
-        c = two()
-        r = a + b + c
+        print ("select name,des,slug from scheme where age='"+a+"' or landsize='"+b+"'")
+        schemeSet1 = Scheme.query.filter(or_(Scheme.age==a, Scheme.landsize==b)).all()
+        schemeSet2 = one(result)
+        schemeSet3 = two(result)
+        r = schemeSet1 + schemeSet2 + schemeSet3
         x = len(r)
-        mycursor.close()
-        return render_template("search.html",r=r,x=x,params=params)
+        return render_template("search.html",r=r,x=x)
 
-def one():
-    mycursor=connection.cursor()
-    if request.method=='POST':
-        result = request.form
-        a = result['kcc']
-        if a == "No":
-            a  = "1"
-        else:
-            a = "2"
-        b = result['rw']
-        if b == "No":
-            b  = "1"
-        else:
-            b = "2"
-        c = result['soilhealth']
-        if c == "No":
-            c  = "1"
-        else:
-            c = "2"
-        mycursor.execute("select name,des,slug from scheme where kcc='"+a+"' or rw='"+b+"' or soilhealth='"+c+"'")
-        r = mycursor.fetchall()
-        mycursor.close()
-        return r
 
-def two():
-    mycursor=connection.cursor()
-    if request.method=='POST':
-        result = request.form
-        a = result['farming']
-        if a == "Organic farming":
-            a  = "organic"
-        elif a == "Dairy farming":
-            a = "dairy"
-        elif a == "Fishery":
-            a = "fish"
-        else:
-            a = "no"
-        b = result['failure']
-        if (b == "Natural Calamities" or b == "Insects" or b == "Other reason"):
-            b  = "1"
-        else:
-            b = "2"
-        c = result['cropins']
-        if c == "No":
-            c = "1"
-        else:
-            c = "2"
-        mycursor.execute("select name,des,slug from scheme where types='"+a+"' or fc='"+b+"' or cropins='"+c+"'")
-        r = mycursor.fetchall()
-        mycursor.close()
-        return r
+def one(result):
+    # result = request.form
+    a = result['kcc']
+    if a == "No":
+        a  = "1"
+    else:
+        a = "2"
+    b = result['rw']
+    if b == "No":
+        b  = "1"
+    else:
+        b = "2"
+    c = result['soilhealth']
+    if c == "No":
+        c  = "1"
+    else:
+        c = "2"
+    r = Scheme.query.filter(or_(Scheme.kcc==a, Scheme.rw==b, Scheme.soilhealth==c)).all()
+    return r
+
+def two(result):
+    a = result['farming']
+    if a == "Organic farming":
+        a  = "organic"
+    elif a == "Dairy farming":
+        a = "dairy"
+    elif a == "Fishery":
+        a = "fish"
+    else:
+        a = "no"
+    b = result['failure']
+    if (b == "Natural Calamities" or b == "Insects" or b == "Other reason"):
+        b  = "1"
+    else:
+        b = "2"
+    c = result['cropins']
+    if c == "No":
+        c = "1"
+    else:
+        c = "2"
+    r = Scheme.query.filter(or_(Scheme.types==a, Scheme.fc==b, Scheme.cropins==c)).all()
+    return r
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
@@ -174,6 +151,7 @@ def contact():
         entry = Contacts(name=name, email = email, subject = subject, msg = message, date= datetime.now() )
         db.session.add(entry)
         db.session.commit()
-    return render_template('contact.html', params=params)
+    return render_template('contact.html')
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
